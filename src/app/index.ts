036f95192e0864265a1f8041eaee5dc1c6d7dd60
gouravindoriya <gouravindoriya00@gmail.com>
2026-04-27 00:54:01 +0530

    i forget to add cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,25 +2,17 @@ import express from 'express'
 import type { Express } from 'express'
 import {authRouter} from '../auth/auth.routes.js'
 import {isauthenticated} from '../comman/auth.middleware/auth.middlerware.js'
+import cors from 'cors'
 function createExpressApplication():Express{
     const app=express()
-
-
-    // Middleware
+    app.use(cors())
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
 
-   
-
-
-    //routes
     app.use('/auth/',authRouter)
-    
     app.get('/dashboard',isauthenticated,(req,res)=>{
         res.json(req.body)
     })
-
-    //export
     return  app
 
 }
